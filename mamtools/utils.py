fe8531c0cdbcc2022de646489fd3e680d70a6765
Marcus Albertsson <marcus.arubertoson@gmail.com>
2016-01-11 22:20:50 +0100

    reworked select mode added various pivot functions

--- mamtools/utils.py
+++ mamtools/utils.py
@@ -1,11 +1,26 @@
 """
 Various utility functions that doesn't really fit anywhere else.
 """
+import math
+import logging
+
 import maya.api.OpenMaya as api
+
 import maya.cmds as cmds
 import maya.mel as mel
 
 import mampy
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+def tool_select(release=False):
+    if release:
+        mel.eval('dR_DoCmd("selectRelease");')
+    else:
+        mel.eval('dR_DoCmd("selectPress");')
 
 
 class ComponentKeep(object):
@@ -80,25 +95,45 @@ def get_global_var(var):
     return mel.eval('$mampytmp = ${}'.format(var))
 
 
-def select_mode(type):
+def select_mode(type_):
     """
     Selection mode setter, given component or objects set Maya selection
     masks.
     """
-    if type in ['vert', 'edge', 'face']:
-        mode = {
-            'vert': 'dR_modeVert',
-            'edge': 'dR_modeEdge',
-            'face': 'dR_modePoly',
-        }[type]
-        mel.eval("{}".format(mode))
-    elif type == 'map':
-        if (cmds.selectMode(q=True, component=True) and
-                cmds.selectType(q=True, puv=True)):
-            cmds.selectMode(object=True)
-            cmds.selectType(allObjects=True)
-        else:
-            mel.eval('dR_modeUV')
+    def trim_dict():
+        new_dict = {}
+        for k, v in kwargs.iteritems():
+            if k.startswith(type_) or k.lower().endswith(type_):
+                new_dict[k] = v
+                return new_dict
+
+    vert = {
+        'vertex': True,
+        'controlVertex': True,
+        'latticePoint': True,
+        }
+    edge = {
+        'edge': True,
+        'surfaceEdge': True,
+        }
+    face = {
+        'facet': True,
+        'surfaceFace': True,
+        }
+    meshuv = {
+        'polymeshUV': True,
+        'surfaceUV': True,
+    }
+    kwargs = locals()[type_]
+    mode = trim_dict()
+    if (cmds.selectMode(q=True, component=True) and
+            cmds.selectType(q=True, **mode)):
+        cmds.selectMode(object=True)
+        cmds.selectType(allObjects=True, allComponents=False)
+    else:
+        cmds.selectMode(component=True)
+        cmds.selectType(allComponents=False)
+        cmds.selectType(**kwargs)
 
 
 def convert(mode, **args):
@@ -109,110 +144,149 @@ def convert(mode, **args):
     for i in s.itercomps():
         if mode == 'vert':
             cl.append(i.to_vert(**args))
-            select_mode('vert')
-        elif mode == 'edge':
-            cl.append(i.to_edge(**args))
-            select_mode('edge')
-        elif mode == 'face':
-            cl.append(i.to_face(**args))
-            select_mode('face')
-        elif mode == 'map':
-            cl.append(i.to_map(**args))
-            select_mode('map')
+        # elif mode == 'edge':
+        #     cl.append(i.to_edge(**args))
+        # elif mode == 'face':
+        #     cl.append(i.to_face(**args))
+        # elif mode == 'meshuv':
+        #     cl.append(i.to_map(**args))
+
+    select_mode(mode)
     cmds.select(list(cl))
 
 
-def set_pivot():
-    pass
-    # s = mampy.selected()
-    # origo_vec = api.MVector(1, 0, 1)
-    # for each in s.iterdags():
-    #     print cmds.manipMoveContext('Move', e=True,
-    #                                  orientTowards=[10, 49, 49])
-    #     print each.name
-    #     print cmds.manipPivot(each.name, q=True, p=True)
-    # for each in s.iterdags():
-    #     print each.rotatePivot.get()
+def set_pivot_center():
+    cmds.xform(cp=True)
+
+
+def set_pivot(vector=(0, 0, 0)):
+    vec = api.MVector(vector)
+    for each in mampy.ls(sl=True, tr=True, l=True).iterdags():
+        try:
+            each.get_transform().set_pivot(vec)
+        except RuntimeError():
+            raise mampy.InvalidSelection('{} is not valid for function.'
+                                         .format(each.typestr))
+
+
+@mampy.history_chunk()
+def match_pivot_to_object():
+    """
+    Match secondary selection pivots to first object selected.
+    """
+    s, hl = mampy.ordered_selection(tr=True, l=True), mampy.ls(hl=True)
+    if len(s) == 0:
+        if len(hl) > 0:
+            s = hl
+        else:
+            return logger.warn('Nothing Selected.')
+
+    # check driver information
+    dag = s.pop(0)
+    trns = dag.get_transform()
+    piv = trns.get_scale_pivot()
+
+    # set pivot for driven objects
+    for each in s.iterdags():
+        print each, type(each)
+        trns = each.get_transform().set_pivot(piv)
+
+
+@mampy.history_chunk()
+def bake_pivot():
+    """
+    Bake modified manipulator pivot onto object.
+    """
+    s = mampy.ordered_selection(tr=True, l=True)
+    dag = s.pop(len(s)-1)
+    out_idx = mampy.get_outliner_index(dag)
+    parent = dag.get_parent()
+
+    # Get manipulator information
+    pos = cmds.manipMoveContext('Move', q=True, p=True)
+    rot = cmds.manipMoveContext('Move', q=True, oa=True)
+    rot = tuple(math.degrees(i) for i in rot)
+
+    # Create dummpy parent
+    space_locator = cmds.spaceLocator(name='bake_dummy_loc', p=pos)[0]
+    cmds.xform(space_locator, cp=True)
+    cmds.xform(space_locator, rotation=rot, ws=True)
+
+    dag.set_parent(space_locator)
+    cmds.makeIdentity(dag.name, rotate=True, translate=True, apply=True)
+
+    dag.set_parent(parent)
+    cmds.delete(space_locator)
+    cmds.reorder(dag.name, f=True); cmds.reorder(dag.name, r=out_idx)
+    cmds.select(list(s), r=True); cmds.select(dag.name, add=True)
 
 
 if __name__ == '__main__':
-    select_mode('map')
-    # cmds.selectMode(object=True)
-    # cmds.selectType(allObjects=True)
+    pass
 
-    # vertex_mask()
-    # edge_mask()
-    # s = mampy.selected()
-    # print s.itercomps().next().typestr
+# def getViewSize():
+#     '''
+#     Gets the size of the pane under the cursor and retruns
+#     it as a array index 0 is x axis and index 1 is y axis
+#     '''
+#     viewPanel = cmds.getPanel(underPointer=True)
+#     size = [0, 0]
+#     size[0] = cmds.control(viewPanel, q=True, w=True)
+#     size[1] = cmds.control(viewPanel, q=True, h=True)
+#     return size
 
-    # fbx_batch_import('G:/wuncook/dlc/dlc14/data/characters/models/geralt/'
-    #                  'armor/armor__skellige/export/dlc/dlc14/data/characters/'
-    #                  'models/geralt/armor/armor__skellige/')
-    # clean_empty_transforms()
+# def polyChamferVtx(doHist=True, width=0.5, deleteFace=False):
+#     '''
+#     Recreation of the polyChamferVtx from mel
+#     performs a vertex extrude, than locks the length and division values and deletes the vtx
+#     to emulate a vertex chamfer
 
-    # t = [0.18677182069146364, 1.344467805788809, -0.6292614505580241]
+#     Args:
+#         dohist(boolean): create history or not
+#         width(float): the width of the vertex chamfer
+#         deleteFace(boolean): delete result face or not.
 
-    # cmds.manipMoveContext('Move', e=True, mode=6)
-    # cmds.manipMoveContext('Move', e=True, oa=t)
+#     Returns:
+#         node(string): the resulting node
+#     '''
+#     result = cmds.polyExtrudeVertex(ch=doHist, length=0, divisions=1, width=width)
+#     if len(result) > 0:
+#         node = [None]
+#         cmds.setAttr(result[0] + '.divisions', lock=True)
+#         cmds.setAttr(result[0] + '.length', lock=True)
+#         node[0] = cmds.rename(result, 'polyChamfer#')
+#     if deleteFace == 1:
+#         getFaces()
+#         cmds.delete()
+#     else:
+#         cmds.DeleteVertex()
+#     return node
 
-    # create script Ctx
-    # cmds.scriptCtx()
-    # cmds.setToolTo()
+# def uvShellHardEdges():
+#     '''
+#     Sets uv border edges on a mesh has hard, and everythign else as soft.
+#     '''
+#     objList = cmds.ls(sl=True, o=True)
+#     finalBorder = []
 
-    # create script job
-    # cmds.scriptJob()
+#     for subObj in objList:
+#         cmds.select(subObj, r=True)
+#         cmds.polyNormalPerVertex(ufn=True)
+#         cmds.polySoftEdge(subObj, a=180, ch=1)
+#         cmds.select(subObj + '.map[*]', r=True)
 
-    # for i in s.itercomps():
+#         polySelectBorderShell(borderOnly=True)
 
-    #     bboxc = list(i.bounding_box.center)[:-1]
-    #     t = cmds.spaceLocator(name='test', p=bboxc)
-    #     node = mampy.DagNode(t.pop())
-    #     cmds.xform(node.name, cp=True)
+#         uvBorder = cmds.polyListComponentConversion(te=True, internal=True)
+#         uvBorder = cmds.ls(uvBorder, fl=True)
 
-    #     verts = i.to_vert()
-    #     n = verts.mesh.getVertexNormals(True, api.MSpace.kWorld)
-    #     tmpn = api.MVector()
-    #     for v in verts.indices:
-    #         tmpn += api.MVector(n[v])
+#         for curEdge in uvBorder:
+#             edgeUVs = cmds.polyListComponentConversion(curEdge, tuv=True)
+#             edgeUVs = cmds.ls(edgeUVs, fl=True)
 
-    #     normal = tmpn/len(verts)
-    #     # new_n = i.bounding_box.center + normal
-    #     # print new_n
-    #     # nnode = mampy.DagNode(cmds.spaceLocator(name='ntest',
-    #                              p=list(new_n)[:-1]).pop())
-    #     # cmds.xform(nnode.name, cp=True)
+#             if len(edgeUVs) > 2:
+#                 finalBorder.append(curEdge)
 
-    #     # cmds.aimConstraint(nnode.name, node.name, n='MTool')
-    #     # cmds.delete(nnode.name)
-    #     cmds.manipMoveContext('Move', e=True, mode=6)
-    #     cmds.manipMoveContext('Move', e=True, aa=normal)
+#         cmds.polySoftEdge(finalBorder, a=0, ch=1)
 
-    #     i.mesh.getFaceNormalIds()
-
-    # move = mampy.mel_globals['gMove']
-    # move_ctx = cmds.superCtx(move, q=True)
-    # print move_ctx
-    # vec = api.MVector()
-    # for i in s.itercomps():
-
-    #     mesh = i.mesh
-    #     for idx in i.indices:
-    #         mesh.getVertexNormal
-    #         vec += mesh.getPolygonNormal(idx, api.MSpace.kWorld)
-
-    # cmds.manipMoveContext('Move', e=True, aa=vec / 3)
-    # print repr(n)
-
-    # rotate = i.rotate.get().pop()
-    # print rotate
-    # print mel.eval('whatIs "Pivot"')
-    # print move
-
-    # m = mampy.MelGlobals()
-    # print m['gMove']
-    # print mel.eval('env')
-    # move = get_global_var("$gMove")
-    # movectx = cmds.superCtx(move, q=True)
-    # print cmds.contextInfo(movectx, q=True, c=True)
-
-    # print cmds.manipMoveContext(, e=True, ot=[-19.9, 11.2, 11.6])
+#     cmds.select(objList)
