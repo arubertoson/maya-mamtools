d15e840b59967ee877ef78514c430fb8b732dea8
Marcus Albertsson <marcus.arubertoson@gmail.com>
2016-01-11 22:15:27 +0100

    updated merge functions

--- mamtools/delete.py
+++ mamtools/delete.py
@@ -1,19 +1,22 @@
 import logging
 import collections
+from functools import partial
 
 import maya.cmds as cmds
-from maya.api.OpenMaya import MFn
+import maya.api.OpenMaya as api
 
 import mampy
+import mamtools
 
 logger = logging.getLogger(__name__)
 
 
-__all__ = ['delete', 'history', 'collapse', 'merge_faces', 'transforms']
+__all__ = ['delete', 'history', 'collapse', 'merge_faces', 'merge_verts',
+           'transforms']
 
 
 @mampy.history_chunk()
-def delete():
+def delete(cv=False):
     """Custom delete using 'right' delete function depending on selection."""
     s = mampy.selected()
     if not s:
@@ -24,13 +27,13 @@ def delete():
             cmds.delete(str(comp.dagpath))
 
         elif comp.is_face():
-            cmds.polyDelFacet(list(comp))
+            cmds.polyDelFacet(list(comp), ch=False)
         elif comp.is_edge():
-            cmds.polyDelEdge(list(comp))
+            cmds.polyDelEdge(list(comp), ch=False, cv=cv)
         elif comp.is_vert():
-            cmds.polyDelVertex(list(comp))
+            cmds.polyDelVertex(list(comp), ch=False)
         else:
-            cmds.delete(str(comp))
+            cmds.delete(str(comp), ch=False)
 
 
 def history():
@@ -59,14 +62,16 @@ def collapse():
 def merge_faces():
     """Removes edges inside of face selection."""
     s = mampy.selected()
-    if not s or not isinstance(next(s.itercomps()), mampy.MeshPolygon):
-        return logger.warn('No polygon selected.')
+    if (not s or not isinstance(next(s.itercomps()), mampy.MeshPolygon) or
+            not len(s[0]) > 1):
+        return logger.warn('Invalid Selection, must have 2 or more polygons'
+                           'selected.')
 
     faces = mampy.SelectionList()
     for comp in s.itercomps():
         border_verts = comp.to_vert(border=True)
         internal_edges = comp.to_edge(internal=True)
-        cmds.polyDelEdge(list(internal_edges), cv=False)
+        cmds.polyDelEdge(list(internal_edges), cv=False, ch=False)
 
         # Find new face
         face = collections.Counter()
@@ -78,16 +83,54 @@ def merge_faces():
     cmds.select(list(faces))
 
 
+@mampy.history_chunk()
+def merge_verts(move):
+    """
+    Merges verts to first selection.
+    """
+    if move:
+        s = mampy.ordered_selection(fl=True)
+        pos = s[0].points[0]
+
+        cmds.xform(list(s), ws=True, t=list(pos)[:3])
+    else:
+        s = mampy.selected()
+    cmds.polyMergeVertex(list(s), distance=0.001, ch=True)
+
+
+def merge():
+    """
+    Dispatches function call depending on selection type
+    """
+    s = mampy.selected()
+    if not s:
+        logger.warn('Nothing Selected.')
+
+    obj = s[0]
+    t = obj.type
+    if obj.type in [api.MFn.kTransform]:
+        t = obj.get_shape().type
+
+    try:
+        {
+            api.MFn.kMeshVertComponent: partial(merge_verts, True),
+            api.MFn.kMeshPolygonComponent: merge_faces,
+            api.MFn.kMesh: mamtools.mesh.combine_separate,
+        }[t]()
+    except KeyError:
+        logger.warn('{} is not a valid type'.format(t))
+
+
 def transforms(translate=False, rotate=False, scale=False):
     """Small function to control transform freezes."""
-    s = mampy.selected()
-    h = mampy.ls(hl=True, dag=True)
-    if h: s.extend(h)
+    s, h = mampy.selected(tr=True, l=True), mampy.ls(hl=True, dag=True)
+    if h:
+        s = h
 
-    transforms = [str(dp) for dp in s.iterdags() if dp.type == MFn.kTransform]
+    transforms = [str(dp) for dp in s.iterdags() if dp.type == api.MFn.kTransform]
     cmds.makeIdentity(transforms, t=translate, r=rotate, s=scale, apply=True)
 
 
 if __name__ == '__main__':
-    history()
+    merge_faces()
 
