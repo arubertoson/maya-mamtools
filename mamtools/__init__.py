52c23039415c12d7565ee0f765065a7464798d20
Marcus Albertsson <marcus.arubertoson@gmail.com>
2016-01-21 14:55:02 +0100

    updated info

--- mamtools/__init__.py
+++ mamtools/__init__.py
@@ -1,28 +1,19 @@
-# -*- coding: utf-8 -*-
-
 """
-    mamtools
-    ~~~~~~~~
+Mamtools Maya scripts.
 
-    mamtools is a toolbox created for Autodesk Maya. Supports custom
-    extensions and contains various improvements to basic tools in
-    maya.
+mamtools is a toolbox created for Autodesk Maya. Supports custom
+extensions and contains various improvements to basic tools in
+maya.
 
-    :copyright: (c) 2015 by Marcus Albertsson
-    :license: MIT, see LICENSE for details
-
-    TODO: License
+:copyright: (c) 2016 by Marcus Albertsson
+:license: MIT, see LICENSE for details
 """
 
-__title__ = "mamtools"
-__author__ = "Marcus Albertsson"
-__email__ = "marcus.arubertoson@gmail.com"
+__author__ = "Marcus Albertsson <marcus.arubertoson@gmail.com>"
+__copyright__ = 'Copyright 2016 Marcus Albertsson'
 __url__ = "http://github.com/arubertoson/mamtools"
-__version__ = "0.1"
-__license__ = "GNU"
-__description__ = "A toolbox containing various improvements to Maya worflows."
-__copyright__ = 'Copyright 2015 Marcus Albertsson'
-
+__version__ = "0.4.0"
+__license__ = "MIT"
 
 
 from mamtools import (select, mesh, camera, display, delete, uv, utils)
