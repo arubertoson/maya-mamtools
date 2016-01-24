8cd725b26b150747a3f625d32e6f741a770ea891
Marcus Albertsson <marcus.arubertoson@gmail.com>
2016-01-24 17:27:11 +0100

    implemented bevel

--- mamtools/api.py
+++ mamtools/api.py
@@ -8,17 +8,40 @@ if you use the detach from api it will work on uvs aswell.
 import logging
 from functools import partial
 
-from maya import cmds
+from maya import cmds, mel
 import maya.api.OpenMaya as api
 
 import mampy
 
-from mamtools import mesh, delete, uv
+from mamtools import mesh, delete, uv, contexts
 
 logger = logging.getLogger(__name__)
 
 
 __all__ = ['merge', 'detach']
+
+
+def bevel():
+    """
+    Bevel is a collection of functions usually performed when certain states
+    are fulfilled, such as: selection, border edge etc.
+    """
+    s = mampy.selected()
+    if not s:
+        return logger.warn('Nothing Selected.')
+
+    for comp in s.itercomps():
+        cmds.select(list(comp))
+
+        if comp.type == api.MFn.kMeshEdgeComponent:
+            if comp.is_border(comp.index):
+                contexts.extrude()
+            else:
+                contexts.bevel()
+        elif comp.type == api.MFn.kMeshPolygonComponent:
+            contexts.extrude()
+        elif comp.type == api.MFn.kMeshVertComponent:
+            contexts.extrude()
 
 
 def merge():
@@ -27,7 +50,7 @@ def merge():
     """
     s = mampy.selected()
     if not s:
-        logger.warn('Nothing Selected.')
+        return logger.warn('Nothing Selected.')
 
     obj = s[0]
     t = obj.type
@@ -44,7 +67,7 @@ def merge():
         logger.warn('{} is not a valid type'.format(t))
 
 
-def detach(hierarchy=False, extract=False):
+def detach(extract=False):
     """
     Dispatches function call depening on selection type or type of panel
     under cursor.
@@ -65,4 +88,4 @@ def detach(hierarchy=False, extract=False):
 
 
 if __name__ == '__main__':
-    detach()
+    merge()
