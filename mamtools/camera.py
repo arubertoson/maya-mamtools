cc18289388c8287283828e1a5feb1d50c50dcd0c
Marcus Albertsson <marcus.arubertoson@gmail.com>
2016-07-10 22:57:47 +0200

    Various fixes in fix selection and added new reset camera function.

--- mamtools/camera.py
+++ mamtools/camera.py
@@ -6,6 +6,8 @@ import maya.cmds as cmds
 import maya.mel as mel
 
 import mampy
+from mampy.nodes import Camera
+from mampy.utils import mvp
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -26,42 +28,52 @@ def fit_selection(mode=None, add=True):
     if mode is None:
         fit_view_history.push_selection(s)
     elif mode == 'next':
-        cmds.select(fit_view_history.jump_forward(s), r=True)
+        fit_view_history.jump_forward(s)
+        cmds.select(fit_view_history.current_element, r=True)
     elif mode == 'back':
-        cmds.select(fit_view_history.jump_back(s), r=True)
+        fit_view_history.jump_back(s)
+        cmds.select(fit_view_history.current_element, r=True)
+    elif mode == 'last':
+        print 'last'
+        last_selected = mampy.ordered_selection()
+        fit_view_history.push_selection(last_selected[-1])
+        cmds.select(list(last_selected[-1]), r=True)
+        cmds.viewFit(f=0.75)
+        cmds.select(list(s))
+        return
 
     cmds.viewFit(f=0.75)
     if mode is not None and add:
         cmds.select(list(s), add=True)
 
 
-def viewport_snap():
+def viewport_snap(fit=True):
     """ZBrush style camera snapping."""
     cameras = cmds.listCameras(o=True)
 
     if 'bottom' not in cameras:
-        top = mampy.Camera('top')
+        top = Camera('top')
 
         new_camera = mampy.DagNode(cmds.duplicate('top', name='bottom').pop())
-        new_camera['translateY'] = top.translateY*-1
-        new_camera['rotateX'] = top.rotateX*-1
+        new_camera['translateY'] = top.translateY * -1
+        new_camera['rotateX'] = top.rotateX * -1
 
     if 'back' not in cameras:
-        back = mampy.Camera('front')
+        back = Camera('front')
 
         new_camera = mampy.DagNode(cmds.duplicate('front', name='back').pop())
-        new_camera['translateZ'] = back.translateZ*-1
+        new_camera['translateZ'] = back.translateZ * -1
         new_camera['rotateY'] = -180
 
     if 'left' not in cameras:
-        side = mampy.Camera('side')
+        side = Camera('side')
 
         new_camera = mampy.DagNode(cmds.duplicate('side', name='left').pop())
-        new_camera['translateX'] = side.translateX*-1
-        new_camera['rotateY'] = side.rotateY*-1
+        new_camera['translateX'] = side.translateX * -1
+        new_camera['rotateY'] = side.rotateY * -1
 
     view = mampy.Viewport.active()
-    camera = mampy.Camera(view.camera)
+    camera = Camera(view.camera)
     if not camera.name.startswith('persp'):
         return cmds.lookThru(view.panel, 'persp')
 
@@ -69,12 +81,14 @@ def viewport_snap():
     main_vector = camera.get_view_direction()
     camera_vector = {}
     for cam in cameras:
-        c = mampy.Camera(cam)
+        c = Camera(cam)
         vec = c.get_view_direction()
         camera_vector[c] = main_vector * vec
 
     cam = max(camera_vector, key=camera_vector.get)
     cmds.lookThru(view.panel, cam.name)
+    if fit:
+        cmds.viewFit(all=True)
 
 
 def maximize_viewport_toggle():
@@ -85,21 +99,25 @@ def maximize_viewport_toggle():
     mel.eval('panePopAt({}, {})'.format(pos.x(), pos.y()))
 
 
+def reset_camera(camera=None):
+    """Reset camera in to default values."""
+    active = mvp.Viewport.active()
+    camera = camera or active.camera
+
+    cmds.xform(camera, os=True, rp=[0, 0, 0])
+    cmds.xform(camera, os=True, sp=[0, 0, 0])
+    rot = cmds.xform(camera, q=True, ws=True, ro=True)
+    trn = cmds.xform(camera, q=True, ws=True, rp=True)
+
+    cmds.xform(camera, ws=True, t=[0, 0, 0])
+    cmds.xform(camera, ws=True, ro=[0, 0, 0])
+
+    rotP = cmds.xform(camera, q=True, ws=True, rp=True)
+    cmds.xform(camera, ws=True, t=[rotP[0] * -1, rotP[1] * -1, rotP[2] * -1])
+    cmds.makeIdentity(apply=True, t=1, r=1, s=1)
+
+    cmds.xform(camera, ws=True, t=[trn[0], trn[1], trn[2]])
+    cmds.xform(camera, os=True, ro=[rot[0], rot[1], rot[2]])
+
 if __name__ == '__main__':
-    viewport_snap()
-    # cmds.select('persp')
-
-    # cmds.xform('persp', os=True, rp=[0,0,0])
-    # cmds.xform('persp', os=True, sp=[0,0,0])
-    # rot = cmds.xform('persp', q=True, ws=True, ro=True)
-    # trn = cmds.xform('persp', q=True, ws=True, rp=True)
-
-    # cmds.xform('persp', ws=True, t=[0,0,0])
-    # cmds.xform('persp', ws=True, ro=[0,0,0])
-
-    # rotP = cmds.xform('persp', q=True, ws=True, rp=True)
-    # cmds.xform('persp', ws=True, t=[rotP[0]*-1, rotP[1]*-1, rotP[2]*-1])
-    # cmds.makeIdentity( apply=True, t=1, r=1, s=1 )
-
-    # cmds.xform('persp', ws=True, t=[trn[0], trn[1], trn[2]])
-    # cmds.xform('persp', os=True, ro=[rot[0], rot[1], rot[2]])
+    reset_camera()
