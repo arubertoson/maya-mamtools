f702c969df155d01c8b51c64998188f1a922b7ea
Marcus Albertsson <marcus.arubertoson@gmail.com>
2016-07-10 23:02:32 +0200

    Fixed various bugs and updated to work with new mampy.

--- mamtools/uv.py
+++ mamtools/uv.py
@@ -6,8 +6,13 @@ import maya.cmds as cmds
 import maya.api.OpenMaya as api
 
 import mampy
+from mampy.selections import SelectionList
+from mampy.components import MeshPolygon, MeshMap
+from mampy.utils import undoable, repeatable, grouped
+from mampy.datatypes import BoundingBox, Line2D
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 class UV3DArea(object):
@@ -15,7 +20,7 @@ class UV3DArea(object):
     def __init__(self, comp):
 
         if not comp:
-            faces = mampy.MeshPolygon(comp.dagpath).get_complete()
+            faces = MeshPolygon(comp.dagpath).get_complete()
         else:
             faces = comp.to_face()
 
@@ -40,16 +45,16 @@ class UV3DArea(object):
         Return uv area of given face index.
         """
         verts = self.get_polygon_vert(idx)
-        vert_len = len(verts)-1
+        vert_len = len(verts) - 1
 
         au, av = self.get_polygon_uv(idx, 0)
         for i in xrange(1, vert_len):
-            if i+1 > vert_len:
+            if i + 1 > vert_len:
                 break
             bu, bv = self.get_polygon_uv(idx, i)
-            cu, cv = self.get_polygon_uv(idx, i+1)
-            s = (bu-au) * (cv-av)
-            t = (cu-au) * (bv-av)
+            cu, cv = self.get_polygon_uv(idx, i + 1)
+            s = (bu - au) * (cv - av)
+            t = (cu - au) * (bv - av)
             self.uv += abs((s - t) * 0.5)
 
     def _get_surface_area(self, idx):
@@ -57,14 +62,14 @@ class UV3DArea(object):
         Return surface area of given face index.
         """
         verts = self.get_polygon_vert(idx)
-        vert_len = len(verts)-1
+        vert_len = len(verts) - 1
 
         a = self.points[verts[0]]
         for i in xrange(1, vert_len):
-            if i+1 > vert_len:
+            if i + 1 > vert_len:
                 break
             b = self.points[verts[i]]
-            c = self.points[verts[i+1]]
+            c = self.points[verts[i + 1]]
 
             la = a.distanceTo(b)
             lb = a.distanceTo(c)
@@ -76,7 +81,7 @@ class UV3DArea(object):
             lc = self.internal_distance(lc)
 
             s = (la + lb + lc) * 0.5
-            self.surface += math.sqrt(s * (s - la)*(s - lb)*(s - lc))
+            self.surface += math.sqrt(s * (s - la) * (s - lb) * (s - lc))
 
     def update(self):
         self.uv = 0.0
@@ -103,43 +108,52 @@ def get_texel_density(areas=None, texture_size=1024):
 
 
 def set_texel_density(shell=True, target_density=0, texture_size=1024):
-    areas = get_areas()
+    if shell:
+        areas = [UV3DArea(c) for c in get_shells()]
+    else:
+        selected = mampy.selected()
+        components = SelectionList()
+        components.extend([c.get_complete() for c in selected.itercomps()])
+        areas = [UV3DArea(c) for c in get_shells(components)]
+
     if target_density == 0:
         target_density = get_average_texel_density(areas, texture_size)
 
+    print areas
     for area in areas:
-        scale_value = (area.ratio*texture_size) / target_density
+        scale_value = (area.ratio * texture_size) / target_density
+        print scale_value
 
-        # Translate shell
         uvs = area.comp.to_map()
-        if shell or not uvs.is_complete():
-            uvs = uvs.get_complete()
-
+        print uvs
         point = uvs.bounding_box.center
         uvs.translate(su=scale_value, sv=scale_value, pu=point.u, pv=point.v)
 
 
-def get_shells():
+def get_shells(components=None):
     """
     Collect selected uv shells.
     """
-    s = mampy.selected()
+    s = components or mampy.selected()
     if not s:
         h = mampy.ls(hl=True)
         if not h:
             return logger.warn('Nothing selected.')
         s.extend(h)
 
-    shells = mampy.SelectionList()
+    shells = SelectionList()
     for c in s.itercomps():
         if not c:
-            c = mampy.MeshMap(c.dagpath).get_complete()
+            print 'test'
+            c = MeshMap(c.dagpath).get_complete()
+        else:
+            c = c.to_map()
 
         count, array = c.mesh.getUvShellsIds()
         wanted = set([array[idx] for idx in c.indices])
 
         for each in wanted:
-            shell = mampy.MeshMap.create(c.dagpath)
+            shell = MeshMap.create(c.dagpath)
             shell.add([idx for idx, num in enumerate(array) if num == each])
             shells.append(shell)
     return list(shells.itercomps())
@@ -174,7 +188,7 @@ class AlignUV(object):
     @property
     def bbox(self):
         if self._bbox is None:
-            self._bbox = mampy.BoundingBox()
+            self._bbox = BoundingBox()
             self._bbox.boxtype = '2D'
             for shell in self.shells:
                 self._bbox.expand(shell.bounding_box)
@@ -209,9 +223,15 @@ class AlignUV(object):
         return self._min_height
 
     @property
-    def span(self):
+    def spanu(self):
         if self._span is None:
-            self._span = self.bbox.width/(len(self.shells)-1)
+            self._span = self.bbox.width / (len(self.shells) - 1)
+        return self._span
+
+    @property
+    def spanv(self):
+        if self._span is None:
+            self._span = self.bbox.height / (len(self.shells) - 1)
         return self._span
 
     @property
@@ -222,7 +242,8 @@ class AlignUV(object):
         return self._shell_sum
 
 
-@mampy.history_chunk()
+@undoable
+@repeatable
 def align(mode):
     """
     Aligns uvs given mode.
@@ -244,7 +265,8 @@ def align(mode):
         shell.translate(**_get_align_kwargs(shell, align))
 
 
-@mampy.history_chunk()
+@undoable
+@repeatable
 def scalefit(mode):
     """
     Docstring
@@ -267,22 +289,23 @@ def scalefit(mode):
 
 def _get_align_kwargs(shell, align):
     return {
-        align.MAX_U: {'u': align.bbox.max.u-shell.bounding_box.max.u},
-        align.MIN_U: {'u': align.bbox.min.u-shell.bounding_box.min.u},
-        align.MAX_V: {'v': align.bbox.max.v-shell.bounding_box.max.v},
-        align.MIN_V: {'v': align.bbox.min.v-shell.bounding_box.min.v},
+        align.MAX_U: {'u': align.bbox.max.u - shell.bounding_box.max.u},
+        align.MIN_U: {'u': align.bbox.min.u - shell.bounding_box.min.u},
+        align.MAX_V: {'v': align.bbox.max.v - shell.bounding_box.max.v},
+        align.MIN_V: {'v': align.bbox.min.v - shell.bounding_box.min.v},
 
-        align.CENTER_U: {'u': align.bbox.center.u-shell.bounding_box.center.u},
-        align.CENTER_V: {'v': align.bbox.center.v-shell.bounding_box.center.v},
+        align.CENTER_U: {'u': align.bbox.center.u - shell.bounding_box.center.u},
+        align.CENTER_V: {'v': align.bbox.center.v - shell.bounding_box.center.v},
 
-        align.SCALE_MAX_U: {'su': align.max_width/shell.bounding_box.width},
-        align.SCALE_MIN_U: {'su': align.min_width/shell.bounding_box.width},
-        align.SCALE_MAX_V: {'sv': align.max_height/shell.bounding_box.height},
-        align.SCALE_MIN_V: {'sv': align.min_height/shell.bounding_box.height},
+        align.SCALE_MAX_U: {'su': align.max_width / shell.bounding_box.width},
+        align.SCALE_MIN_U: {'su': align.min_width / shell.bounding_box.width},
+        align.SCALE_MAX_V: {'sv': align.max_height / shell.bounding_box.height},
+        align.SCALE_MIN_V: {'sv': align.min_height / shell.bounding_box.height},
     }[align.mode]
 
 
-@mampy.history_chunk()
+@undoable
+@repeatable
 def distribute(mode):
     """
     docstring.
@@ -300,15 +323,16 @@ def distribute(mode):
         if idx == 0:
             distance = getattr(align.bbox.min, mode)
             continue
-        elif idx == len(align.shells)-1:
+        elif idx == len(align.shells) - 1:
             continue
 
-        distance += align.span
-        translate_value = distance-getattr(shell.bounding_box.center, mode)
+        distance += align.spanu if mode == 'u' else align.spanv
+        translate_value = distance - getattr(shell.bounding_box.center, mode)
         shell.translate(**{mode: translate_value})
 
 
-@mampy.history_chunk()
+@undoable
+@repeatable
 def space(mode, space=0.04):
     try:
         space_mode = {
@@ -320,19 +344,20 @@ def space(mode, space=0.04):
 
     align = AlignUV(space_mode)
 
-    spacing = (space*(len(align.shells)-1)+align.shell_sum)/2
-    calculated_space = getattr(align.bbox.center, mode)-spacing
+    spacing = (space * (len(align.shells) - 1) + align.shell_sum) / 2
+    calculated_space = getattr(align.bbox.center, mode) - spacing
 
     for shell in align.shells:
         offset = shell.bounding_box
-        shell.translate(**{mode: calculated_space-getattr(offset.min, mode)})
+        shell.translate(**{mode: calculated_space - getattr(offset.min, mode)})
 
         max_ = getattr(offset.max, mode)
         min_ = getattr(offset.min, mode)
-        calculated_space += space+(max_-min_)
+        calculated_space += space + (max_ - min_)
 
 
-@mampy.history_chunk()
+@undoable
+@repeatable
 def tear_off():
     """
     Creates a new uv shell from selected faces.
@@ -343,80 +368,13 @@ def tear_off():
         if not comp.is_face():
             continue
 
-        faces = comp.to_edge(border=True)
-        cmds.polyMapCut(list(faces))
+        edges = comp.to_edge(border=True)
+        cmds.polyMapCut(list(edges))
     cmds.select(list(s))
 
 
-def get_angle(pointA, pointB):
-    """
-    Calculates the angle between two points.
-    """
-
-    if pointA.x >= pointB.x:
-        distX = (pointA.x - pointB.x)
-        distY = (pointA.y - pointB.y)
-    else:
-        distX = (pointB.x - pointA.x)
-        distY = (pointB.y - pointA.y)
-
-    angle = math.degrees(math.atan2(distY, distX))
-    if angle == 0.0000 or angle == 90.0000 or angle == -90.0000:
-        angle = 0
-
-    elif angle >= -44.9999 and angle <= 44.9999:
-        angle *= -1
-
-    elif angle >= 45.0001 and angle <= 89.9999:
-        angle = 90 - angle
-
-    elif angle <= -45.0001 and angle >= -89.9999:
-        angle = (90 + angle) * -1
-
-    else:
-        angle = 45
-
-    return angle
-
-
-class Line2D(object):
-    """
-    Line2D class is the line between two points.
-    """
-
-    def __init__(self, pointA, pointB):
-
-        self.pointA, self.pointB = pointA, pointB
-
-        self.vA = self.vectorA = api.MVector(pointA)
-        self.vB = self.vectorB = api.MVector(pointB)
-
-        self._angle = None
-        self._center = None
-
-    def __repr__(self):
-        return 'Line({pointA!r}, {pointB!r}'.format(**self.__dict__)
-
-    def __str__(self):
-        return '{pointA}, {pointB}'.format(**self.__dict__)
-
-    def __iter__(self):
-        return iter(self.pointA, self.pointB)
-
-    @property
-    def angle(self):
-        if self._angle is None:
-            self._angle = get_angle(self.vA, self.vB)
-        return self._angle
-
-    @property
-    def center(self):
-        if self._center is None:
-            x, y, z = (self.vA + self.vB) / 2
-            self._center = mampy.Point2D(x, y)
-        return self._center
-
-
+@undoable
+@repeatable
 def orient():
     """
     Orients shell to closest 90 degree angle on selection.
@@ -425,32 +383,43 @@ def orient():
     if not s:
         return logger.warn('Nothing selected.')
 
+    if s[0].is_map():
+        new, current = SelectionList(), list(s)
+        print current
+        for pair in grouped(current, 2):
+            new.append(MeshMap(pair))
+        s = new
+
     for comp in s.itercomps():
 
-        uvs = comp.to_map()
-        if len(uvs) > 2:
+        if not comp.is_map():
+            comp = comp.to_map()
+        if len(comp) > 2:
             logger.warn('Does not support border edges or multiple selections.')
             continue
 
-        line = Line2D(*uvs.points)
-        shell = uvs.get_uv_shell()
+        line = Line2D(*comp.points)
+        shell = comp.get_uv_shell()
         shell.translate(a=line.angle, pu=line.center.u, pv=line.center.v)
 
 
-@mampy.history_chunk()
+@undoable
+@repeatable
 def translate(u=0.0, v=0.0):
     for shell in get_shells():
         shell.translate(u=u, v=v)
 
 
-@mampy.history_chunk()
+@undoable
+@repeatable
 def rotate(angle):
     for shell in get_shells():
         point = shell.bounding_box.center
         shell.translate(angle=angle, pu=point.u, pv=point.v)
 
 
-@mampy.history_chunk()
+@undoable
+@repeatable
 def mirror(mode):
     try:
         mirror = {
@@ -465,4 +434,4 @@ def mirror(mode):
         shell.translate(**{mirror: -1, 'pu': point.u, 'pv': point.v})
 
 if __name__ == '__main__':
-    align('maxu')
+    set_texel_density(shell=False)
